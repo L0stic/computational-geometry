c90755c0302270cda9b22773820f8ead5acda474
L0stic <k0ndratev.da@yandex.ru>
2022-03-19 12:40:08 +0300

    Hot fix

--- app/src/minimum_disk_check/util.py
+++ app/src/minimum_disk_check/util.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 import numpy as np
 from .visual import plot_circle
 
-TOLERANCE = 1e-14
-
 
 def get_distance(p1: np.array, p2: np.array) -> float:
     diff = p1 - p2
@@ -17,19 +15,20 @@ def get_s_2(a: float, b: float,  c: float) -> float:
 
 @dataclass
 class Disc:
-    def __init__(self, center: np.array = None, radius: float = 0.0):
+    def __init__(self, center: np.array = None, radius: float = 0.0, tolerance: float = 1e-9):
         self.center = center
         self.radius = radius
+        self.TOLERANCE = tolerance
 
     def circumscribe_point(self, p: np.array):
         self.center = p
         self.radius = 0.0
-        print(self.center, self.radius)
+        # print(self.center, self.radius)
 
     def circumscribe_diameter(self, p1: np.array, p2: np.array):
         self.center = (p1 + p2) / 2.0
         self.radius = get_distance(p1, p2) / 2.0
-        print(self.center, self.radius)
+        # print(self.center, "%.4f" % self.radius)
 
     def circumscribe_triangle(self, p1: np.array, p2: np.array, p3: np.array):
         a1 = get_distance(p2, p3)
@@ -37,7 +36,7 @@ class Disc:
         a3 = get_distance(p1, p2)
         # print(a1, a2, a3)
         s_2 = get_s_2(a1, a2, a3)
-        if s_2 < TOLERANCE:
+        if s_2 < self.TOLERANCE:
             print("This is line")
             if a1 > a2 and a1 > a3:
                 self.circumscribe_diameter(p2, p3)
@@ -53,17 +52,17 @@ class Disc:
         k2 = (a2 * a2) * ((p2 - p1) * (p2 - p3)).sum()
         k3 = (a3 * a3) * ((p3 - p1) * (p3 - p2)).sum()
 
-        self.center = (k1 * p1 + k2 * p2 + k3 * p3) / (8 * s_2)
+        self.center = (k1 * p1 + k2 * p2 + k3 * p3) / (8.0 * s_2)
         self.radius = (a1 * a2 * a3) / (4.0 * s)
-        print(self.center, self.radius)
+        # print("[%.14f %.14f]" % (self.center[0], self.center[1]), "%.14f" % self.radius)
         return True
 
     def is_contain(self, point: np.array) -> int:
         distance = get_distance(self.center, point)
         diff = self.radius - distance
-        if diff > TOLERANCE:
+        if diff > self.TOLERANCE:
             return 1
-        elif diff < -TOLERANCE:
+        elif diff < -self.TOLERANCE:
             return -1
         else:
             return 0
@@ -72,7 +71,7 @@ class Disc:
         diff_center = get_distance(self.center, disc.center)
         diff_radius = np.abs(self.radius - disc.radius)
 
-        if diff_center <= TOLERANCE and diff_radius <= TOLERANCE:
+        if diff_center <= self.TOLERANCE and diff_radius <= self.TOLERANCE:
             return True
         else:
             return False
@@ -80,7 +79,7 @@ class Disc:
     def to_string(self):
         if self.center is None:
             return "empty set"
-        elif self.radius <= TOLERANCE:
+        elif self.radius <= self.TOLERANCE:
             return f"point: {self.center}"
         else:
             return f"center: {self.center}, r: {self.radius}"
